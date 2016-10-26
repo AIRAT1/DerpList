51c80ecfde460bf008c91e0ee40d28f6176d1e3b
Ayrat <ayrat1@mail.ru>
2016-10-26 16:03:20 +0300

    2b complete

--- app/src/main/java/de/android/derplist/adapter/DerpAdapter.java
+++ app/src/main/java/de/android/derplist/adapter/DerpAdapter.java
@@ -1,5 +1,58 @@
 package de.android.derplist.adapter;
 
 
-public class DerpAdapter {
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import de.android.derplist.R;
+import de.android.derplist.model.ListItem;
+
+public class DerpAdapter extends RecyclerView.Adapter<DerpAdapter.DerpHolder>{
+    private List<ListItem> listData;
+    private LayoutInflater inflater;
+
+    public DerpAdapter(List<ListItem> listData, Context context) {
+        this.inflater = LayoutInflater.from(context);
+        this.listData = listData;
+    }
+
+
+    @Override
+    public DerpHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.list_item, parent, false);
+        return new DerpHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(DerpHolder holder, int position) {
+        ListItem item = listData.get(position);
+        holder.title.setText(item.getTitle());
+        holder.icon.setImageResource(item.getImageResId());
+    }
+
+    @Override
+    public int getItemCount() {
+        return listData.size();
+    }
+
+    class DerpHolder extends RecyclerView.ViewHolder {
+        private TextView title;
+        private ImageView icon;
+        private View container;
+
+        public DerpHolder(View itemView) {
+            super(itemView);
+
+            title = (TextView)itemView.findViewById(R.id.lbl_item_txt);
+            icon = (ImageView)itemView.findViewById(R.id.im_item_icon);
+            container = itemView.findViewById(R.id.cont_item_root);
+        }
+    }
 }
